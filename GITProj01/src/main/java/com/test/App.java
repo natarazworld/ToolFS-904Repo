9705fd977044475de04bd8c3e408b77ba96dd27f
nataraz <natarazjavaarena@gmail.com>
2025-01-08 09:46:31 +0530

    Arithmetic service is added

--- GITProj01/src/main/java/com/test/App.java
+++ GITProj01/src/main/java/com/test/App.java
@@ -1,10 +1,14 @@
 package com.test;
 
+import com.nt.service.Arithmetic;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Arithmetic  ar=new Arithmetic();
+        int result=ar.sum(10, 20);
+        System.out.println("result::"+result);
     }
 }

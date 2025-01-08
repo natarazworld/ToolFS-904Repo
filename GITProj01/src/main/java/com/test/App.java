9e69dffcf3f8a5ee99ce2bdeaa3a04d3623b8f9f
nataraz <natarazjavaarena@gmail.com>
2025-01-08 10:05:39 +0530

    main class code  change

--- GITProj01/src/main/java/com/test/App.java
+++ GITProj01/src/main/java/com/test/App.java
@@ -8,7 +8,7 @@ import com.nt.service.Arithmetic;
 public class App {
     public static void main(String[] args) {
         Arithmetic  ar=new Arithmetic();
-        int result=ar.sum(10, 20);
+        long result=ar.sum(10, 20);
         System.out.println("result::"+result);
     }
 }

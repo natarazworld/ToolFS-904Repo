6b160cc5a72bb5a65fdd6d3001401521d7402e5c
nataraz <natarazjavaarena@gmail.com>
2025-01-09 10:15:30 +0530

    TL -Branch1 creation

---
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -0,0 +1,10 @@
+package com.nt.upi;
+
+public class UPIPaymentOperations {
+	
+	
+	public   String   doPayment(String upiId,  double amount) {
+		return  amount+"  is transfered  to  "+upiId;
+	}
+
+}

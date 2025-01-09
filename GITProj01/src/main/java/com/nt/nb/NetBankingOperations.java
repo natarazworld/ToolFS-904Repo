336b7cd135f9401f29d60018fd80680f66349bcd
nataraz <natarazjavaarena@gmail.com>
2025-01-09 10:34:14 +0530

    TL-creating Branch2

---
+++ GITProj01/src/main/java/com/nt/nb/NetBankingOperations.java
@@ -0,0 +1,9 @@
+package com.nt.nb;
+
+public class NetBankingOperations {
+
+	public   String doPayment (long cardNo,double amt) {
+		return amt+"  payment is done with the card::"+cardNo;
+	}
+	
+}

a48d12cb9bb830756f602bad3a3959b7aa5a7977
nataraz <natarazjavaarena@gmail.com>
2025-01-09 10:37:29 +0530

    Dev1 added the cashback offers

--- GITProj01/src/main/java/com/nt/nb/NetBankingOperations.java
+++ GITProj01/src/main/java/com/nt/nb/NetBankingOperations.java
@@ -6,4 +6,9 @@ public class NetBankingOperations {
 		return amt+"  payment is done with the card::"+cardNo;
 	}
 	
+	public   String cashbackOffer(String txId, double amt) {
+		return   amt*0.05+" amount goes as the cashback amount";
+		
+	}
+	
 }
